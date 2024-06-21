b0af0404c69ec922813318dccd1d45c924fe6766
Patthawikarn <143491751+Patthawikarn@users.noreply.github.com>
2024-06-21 12:41:41 +0700

    switch api test

--- index.js
+++ index.js
@@ -8,29 +8,25 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
     if(employeeCode.trim() === '' || password.trim() === '') {
       alert('Please enter both employee code and password.');
     } else {
-      fetch("/api/login", { // เรียกไปที่ Proxy Server ที่คุณสร้างขึ้นมา
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          Username: employeeCode,
-          Password: password
-        })
-      })
-      .then(response => response.json())
-      .then(data => {
+      // ตัวอย่างของการตอบกลับสำหรับการเข้าสู่ระบบ
+      var mockApiResponse = {
+        Status: "OK"
+      };
+  
+      // ส่งข้อมูลกลับ
+      setTimeout(() => {
+        handleResponse(mockApiResponse);
+      }, 1000); // จำลองการเชื่อมต่อโดยใช้ setTimeout
+  
+      // ฟังก์ชันจัดการการตอบกลับ
+      function handleResponse(data) {
         if (data.Status == "OK") {
           alert('Login successful!');
           window.location.href = "./main.html";
         } else {
           alert('Login failed. Please try again.');
         }
-      })
-      .catch(error => {
-        console.error('Error:', error);
-        alert('Login failed. Please try again.');
-      });
+      }
     }
   });
   
