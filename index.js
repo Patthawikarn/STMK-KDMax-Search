7b96be791ef5d937ec1a0c015604ae073d500fee
Patthawikarn <143491751+Patthawikarn@users.noreply.github.com>
2024-06-21 12:47:06 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,32 +1,26 @@
 // scripts.js
 document.getElementById('loginForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    
-    var employeeCode = document.getElementById('floatingInput').value;
-    var password = document.getElementById('floatingPassword').value;
-    
-    if(employeeCode.trim() === '' || password.trim() === '') {
-      alert('Please enter both employee code and password.');
-    } else {
-      // ตัวอย่างของการตอบกลับสำหรับการเข้าสู่ระบบ
-      var mockApiResponse = {
-        Status: "OK"
-      };
+  event.preventDefault();
   
-      // ส่งข้อมูลกลับ
-      setTimeout(() => {
-        handleResponse(mockApiResponse);
-      }, 1000); // จำลองการเชื่อมต่อโดยใช้ setTimeout
+  var employeeCode = document.getElementById('floatingInput').value;
+  var password = document.getElementById('floatingPassword').value;
   
-      // ฟังก์ชันจัดการการตอบกลับ
-      function handleResponse(data) {
-        if (data.Status == "OK") {
-          alert('Login successful!');
-          window.location.href = "./main.html";
-        } else {
-          alert('Login failed. Please try again.');
-        }
+  if(employeeCode.trim() === '' || password.trim() === '') {
+    alert('Please enter both employee code and password.');
+  } else {
+    // สำหรับตัวอย่างเราจะใช้ setTimeout เพื่อจำลองการเชื่อมต่อ API ที่จะมีการตอบกลับในอนาคต
+    setTimeout(() => {
+      handleResponse({ Status: "OK" });
+    }, 1000); // จำลองการเชื่อมต่อโดยใช้ setTimeout
+
+    // ฟังก์ชันจัดการของการตอบกลับ
+    function handleResponse(data) {
+      if (data.Status == "OK") {
+        alert('Login successful!');
+        window.location.href = "./main.html";
+      } else {
+        alert('Login failed. Please try again.');
       }
     }
-  });
-  
+  }
+});
