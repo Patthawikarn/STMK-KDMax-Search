a6af600e1f762d7cb1aa00455ac6a76e25571622
Patthawikarn <143491751+Patthawikarn@users.noreply.github.com>
2024-07-01 14:10:29 +0700

    fix login

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
-// scripts.js
 document.getElementById('loginForm').addEventListener('submit', function(event) {
   event.preventDefault();
-  
+
   var employeeCode = document.getElementById('floatingInput').value;
   var password = document.getElementById('floatingPassword').value;
-  
-  if(employeeCode.trim() === '' || password.trim() === '') {
+
+  if (employeeCode.trim() === '' || password.trim() === '') {
     alert('Please enter both employee code and password.');
   } else {
-    // สำหรับตัวอย่างเราจะใช้ setTimeout เพื่อจำลองการเชื่อมต่อ API ที่จะมีการตอบกลับในอนาคต
     setTimeout(() => {
       handleResponse({ Status: "OK" });
-    }, 1000); // จำลองการเชื่อมต่อโดยใช้ setTimeout
+    }, 1000);
 
-    // ฟังก์ชันจัดการของการตอบกลับ
     function handleResponse(data) {
       if (data.Status == "OK") {
         alert('Login successful!');
